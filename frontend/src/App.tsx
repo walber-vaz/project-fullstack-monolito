938bdd175ea16fe76093ebafe65352b7d946fcc2
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-28 21:57:11 -0300

    feat: add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from '@/pages/Home'
+
 export default function App(): JSX.Element {
   return (
-    <main
-      className={`
-      bg-gray-200
-      h-screen
-      w-full
-      flex
-      justify-center
-      items-center
-    `}
-    >
-      <h1>Front end</h1>
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export function Home(): JSX.Element {
+  return (
+    <main
+      className={`
+      bg-gray-200
+      h-screen
+      w-full
+      flex
+      justify-center
+      items-center
+    `}
+    >
+      <h1>Front end</h1>
+    </main>
+  )
+}
